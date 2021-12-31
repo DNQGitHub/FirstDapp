3832ce2e44e47679d44b63948669c91c61bf18bb
DNQ <dnq.httt@gmail.com>
2021-12-31 16:37:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import {WallectConnectConfig} from './configs';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import {
 	Linking,
